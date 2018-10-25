ff77f8b62f1330d56aceece9ac9193b165208fde
ejdelrio <edwinjdelrio@gmail.com>
2018-10-24 22:13:46 -0700

    :D

--- src/Util/CommonWorkItems.js
+++ src/Util/CommonWorkItems.js
@@ -5,20 +5,7 @@ const dotenv = require( dotenv ).config();
 
 class CommonWorkItems
 {
-  static BearerAuthentication( request, response, next )
-  {
 
-  }
-
-  static BasicAuthentication( request, response, next )
-  {
-
-  }
-
-  static ErrorHadnler( request, response, next )
-  {
-
-  }
 }
 
 module.exports = CommonWorkItems;
