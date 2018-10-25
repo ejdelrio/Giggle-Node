c3977fc13f88f62f38f5fc8f5e8c4a1fd844abcb
ejdelrio <edwinjdelrio@gmail.com>
2018-10-24 22:19:16 -0700

    Added scaffolding to CommonWorkItems.js

--- src/Util/CommonWorkItems.js
+++ src/Util/CommonWorkItems.js
@@ -5,7 +5,26 @@ const dotenv = require( dotenv ).config();
 
 class CommonWorkItems
 {
+  static BearerAuthentication( request, response, next )
+  {
+    debug( "Entering BearerAuthentication" );
+    //TODO : Implement
+    debug( "Exiting BearerAuthentication" );
+  }
 
+  static BasicAuthentication( request, response, next )
+  {
+    debug( "Entering BasicAuthentication" );
+    //TODO : Implement
+    debug( "Exiting BasicAuthentication" )
+  }
+
+  static ErrorHandler( request, response, next )
+  {
+    debug( "Entering ErrorHandler" );
+    //TODO : Implement
+    debug( "Exiting ErrorHandler" );
+  }
 }
 
 module.exports = CommonWorkItems;
