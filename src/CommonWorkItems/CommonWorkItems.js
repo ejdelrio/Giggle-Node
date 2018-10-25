cbbffd4a0875f8c18e149288ce75e91e130257f8
ejdelrio <edwinjdelrio@gmail.com>
2018-10-24 22:07:37 -0700

    Added an additional dependancy for error handling

---
+++ src/CommonWorkItems/CommonWorkItems.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const debug = require( "debug" )( "Giggle-Node : CommonWorkItems.js" );
+const dotenv = require( dotenv ).config();
+
+class CommonWorkItems
+{
+  static BearerAuthentication( request, response, next )
+  {
+
+  }
+
+  static BasicAuthentication( request, response, next )
+  {
+
+  }
+
+  static ErrorHadnler( request, response, next )
+  {
+
+  }
+}
+
+module.exports = CommonWorkItems;
