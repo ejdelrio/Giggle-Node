32aa57c7861272020eccc20021dfffd410bd1eba
ejdelrio <edwinjdelrio@gmail.com>
2018-11-18 18:02:50 -0800

    Began writing private functions for PostClientSdkOperation

--- src/SdkOperations/ClientSdkOperations/PostClientSdkOperation.js
+++ src/SdkOperations/ClientSdkOperations/PostClientSdkOperation.js
@@ -3,7 +3,55 @@
 const { SdkOperation } = require( "../SdkOperation" );
 const debug = require( "debug" )( "Giggle-Node : PostClientSdkOperation" );
 const { json } = require( "body-parser" );
+
 const { ClientSchema } = require( "../../Schema/ClientSchema" );
+const { ObjectionHelperSinglton } = require( "../../Util/ObjectionSQLHelper" );
+const { knexConnection } = ObjectionHelperSingleton;
+const { ValidateStringIsNotNullOrWhiteSpace } = require( "../../Util/CommonWorkItems" );
+const createError = require( "http-errors" );
+
+function SignJsonToken()
+{
+
+}
+
+function CreateParameterContainer( request, next )
+{
+    debug( "Creating client parameters" );
+    let { userName, passWord, email } = request;
+
+    try
+    {
+        ValidateStringIsNotNullOrWhiteSpace( userName );
+        ValidateStringIsNotNullOrWhiteSpace( passWord );
+        ValidateStringIsNotNullOrWhiteSpace( email );
+    }
+    catch ( error )
+    {
+        debug( `Error : ${ error.message }` );
+        next( createError( 400, "Invalid Parameters" ) );
+        return {};
+    }
+
+    delete request.passWord;
+    return { userName, passWord, email };
+}
+
+function EncryptPlainTextPassword( postClientParameters )
+{
+
+}
+
+function PostClient( request, response, next )
+{
+    debug( "Entering postClientSdkOperation" );
+    let postClientParameters = CreateParameterContainer( request, next );
+
+    knexConnection( ClientSchema.tableName )
+        .insert( postClientParameters )
+        .then()
+        .catch( error => next( createError( 400, error.message ) ) );
+}
 
 class PostClientSdkOperation extends SdkOperation
 {
@@ -15,12 +63,7 @@ class PostClientSdkOperation extends SdkOperation
 
     Invoke()
     {
-        return function ( request, response, next )
-        {
-            debug( "Entering UserSignupSdkOperation" );
-
-            next();
-        }
+        return PostClient;
     }
 }
 
